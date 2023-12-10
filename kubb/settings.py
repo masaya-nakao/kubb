c96f1222667fe9363aaa877f61a9fb7b7c6331bc
app_admin <masayajin2002@gmail.com>
2023-12-10 14:46:16 +0900

    AllowedHost

--- kubb/settings.py
+++ kubb/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/usr/share/nginx/html/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
